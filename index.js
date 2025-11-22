1fa274672c5b2177ac501374d96663def9c295c4
Alif Hosson <153631607+Alifhosson@users.noreply.github.com>
2025-11-22 20:22:13 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function safeRequire(path) {
+  try {
+    delete require.cache[require.resolve(path)]; // old cache clear
+    require(path);
+    console.log(`✅ ${path} loaded successfully.`);
+  } catch (err) {
+    console.log(`\n❌ ERROR in file: ${path}`);
+    console.log("📛 Error Message:", err.message);
+    console.log("📄 Error Stack:\n", err.stack);
+
+    console.log(`🔁 Retrying ${path} in 5 seconds...\n`);
+
+    setTimeout(() => safeRequire(path), 5000);
+  }
+}
+
+// বটগুলো লোড করুন
+safeRequire('./poton');
+safeRequire('./Seven1Tel');
+
+console.log('Bot1, Bot2, are running...');
